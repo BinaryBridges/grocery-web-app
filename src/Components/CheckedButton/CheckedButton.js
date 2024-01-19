b5e25916bcc738cc918bd8161bf76f19f218894d
NuNathan <nrcsme@gmail.com>
2024-01-19 15:37:15 -0700

    Saving feature implemented

--- src/Components/CheckedButton/CheckedButton.js
+++ src/Components/CheckedButton/CheckedButton.js
@@ -22,6 +22,7 @@ export default function MultiActionAreaCard({image, name}) {
         setButtonText("REMOVE")
         setVariant("outlined")
         setColor("error")
+        setChecked([-1])
       }
     }
   }, [])
@@ -52,12 +53,14 @@ export default function MultiActionAreaCard({image, name}) {
         localStorage.setItem("foodList", JSON.stringify(foodList))
     }
 
+
+
     setChecked(newChecked);
   };
 
   return (
     <Card sx={{ maxWidth: 600 }}>
-      <CardActionArea>
+      <CardActionArea >
         <CardMedia
           component="img"
           height="170"
